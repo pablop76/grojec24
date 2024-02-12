b786fe6bbaca05355585fd0b4a25955528e0776b
pablop76 <pawelpoltoraczyk@gmail.com>
2024-02-12 09:02:20 +0100

    usuniecie komentarzy w js

--- dist/js/app.js
+++ dist/js/app.js
@@ -15,25 +15,23 @@ document.addEventListener("DOMContentLoaded", (event) => {
   });
 // poup wyskakujacy raz dziennie
 const modal = UIkit.modal("#modal-media-image");
-// sprawdza, czy minął jeden dzień.
 function hasOneDayPassed(){
-// pobierz dzisiejszą datę. np.: „11.02.2024”
+
 const date = new Date().toLocaleDateString();
-// jeśli w localstorage znajduje się data i jest ona równa powyższej:
-// wniosek, że dzień jeszcze nie minął, ponieważ obie daty są równe.
+
 if( localStorage.yourapp_date == date ) 
     return false;
-// ta część logiki ma miejsce po upływie dnia
+
 localStorage.yourapp_date = date;
 return true;
 }
-// jakaś funkcja, która powinna działać raz dziennie
+
 function runOncePerDay(){
 if( !hasOneDayPassed() ) return false;
 setTimeout(() => {
   modal.show();
 }, "7000");
 }
-runOncePerDay(); // uruchom kod
+runOncePerDay();
 
 });
